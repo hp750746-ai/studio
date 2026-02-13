96101fed53812b1d2e6e953d1074c3e3af76ed20
harsh prajapati <hp750746@gmail.com>
2026-02-13 14:22:25 +0000

    api AIzaSyCIcnxYRSgVcYdC1bGGlYGPg2NM0UKmKew

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1768454546-ea898",
   "appId": "1:849613203301:web:848a45780d7fc79abfe38c",
-  "apiKey": "AIzaSyDsmX_VngPpFdgxk4uDNG0TRNmGXKvyjFA",
+  "apiKey": "AIzaSyCIcnxYRSgVcYdC1bGGlYGPg2NM0UKmKew",
   "authDomain": "studio-1768454546-ea898.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "849613203301"
